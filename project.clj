55d63bed7b477934bbdcb8bd7f275e9b8bcf7ef1
Gregory Propf <gpropf@gmail.com>
2021-10-23 23:01:46 -0700

    Added clojurescript dependency.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject my-project "1.0.0-SNAPSHOT"
   :description "Minimalist user interface"
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojurescript {:mvn/version "1.10.597"}]  [org.clojure/clojure "1.10.1"]]
   :main mui.core
-  :paths ["src/mui"]
-  )
+  :paths ["src/mui"])
