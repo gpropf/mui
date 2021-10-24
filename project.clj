be0850033cae1d1f7c744d10208870b5220bf89a
Gregory Propf <gpropf@gmail.com>
2021-10-23 22:24:20 -0700

    Bumped the clojure version to 1.10.1

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject my-project "1.0.0-SNAPSHOT"
   :description "Minimalist user interface"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main mui.core
   :paths ["src/mui"]
   )
