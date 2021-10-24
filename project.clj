7f963676bce802a26d79e3036a2059a6b2904d66
Gregory Propf <gpropf@gmail.com>
2021-10-23 22:08:44 -0700

    Added a :paths key

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject my-project "1.0.0-SNAPSHOT"
   :description "Minimalist user interface"
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :main mui.core)
+  :main mui.core
+  :paths ["src/mui"]
+  )
